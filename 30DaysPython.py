14cf58d8643a89c745a00d085751686c8949e981
Fırat Çakır <73741004+FratCR@users.noreply.github.com>
2023-04-02 18:28:07 +0300

    30 Day of the Python notes and examples
    30 day of the python notes and examples, I'm now finished 4. day and starting 5. day

--- 30DaysPython.py
+++ 30DaysPython.py
@@ -4,6 +4,7 @@
 
 # DATA TYPES
 
+"""
 print(type(18))          # Int
 print(type(3.14))        # Float
 print(type(1 + 3j))      # Complex number
@@ -12,9 +13,9 @@ print(type([1, 2, 3]))   # List
 print(type({'name':'Firat'})) # Dictionary
 print(type({9.8, 3.14, 2.7}))    # Set
 print(type((9.8, 3.14, 2.7)))    # Tuple
+"""
 
 # OPERATORS
-
 """
 # we can use 3 nail to the area we don't want it to detect
 addition(+)
@@ -25,7 +26,6 @@ exponential(**)
 modulus(%)
 Floor division operator(//)
 """
-
 # End of the Day 1
 
 # --------------------
@@ -35,15 +35,19 @@ Floor division operator(//)
 # variable names must not start with numbers or special characters or '-' character
 # No spaces when naming a variable
 
+"""
 number1, string1, float1 = 13, "Hi Guys", 2.69 # you can add multiple value in 1 line
 print("Firat saying: ", string1)
 
 name, age = input("What is your name ? "), input("What is your age ? ") # you can want user input and you can add multiple in 1 line again
 print(f"Hello {name}! Welcome to our store. Your age is {age}, right huh {name} ?")
+"""
 
 # CONVERT TYPES
 
 # int to float
+
+"""
 num_int = 10
 print('num_int',num_int)         # 10
 num_float = float(num_int)
@@ -68,6 +72,7 @@ first_name = 'Firat'
 print(first_name) # 'Firat'
 first_name_to_list = list(first_name)
 print(first_name_to_list) # ['F', 'i', 'r', 'a', 't']
+"""
 
 # End of the Day 2
 
@@ -111,6 +116,7 @@ or	        Returns True if one of the statements is true	            x < 5 or x 
 not	        Reverse the result, returns False if the result is true	    not(x < 5 and x < 10)
 """
 
+"""
 # Homework 1: Write a script that prompts the user to enter base and height of the triangle and calculate an area of this triangle
 print("Calculate the area of the triangle by entering the base area and height values (cm)")
 basearea = float(input(" Enter a Base area: "))
@@ -130,10 +136,248 @@ daily_work = float(input("Enter your daily work hour as dollar: "))
 daily_earn = (Rph * daily_work)
 print("Your weekly earned money is: ", daily_earn * 7," USD$")
 print("Your montly earned money is: ", daily_earn * 30, " USD$")
+"""
 
 # End of the Day 3
 
 # -----------------
 
 # Start of the Day 4
+
+# STRINGS
+
+"""
+print('I hope everyone is enjoying the Python Challenge.\nAre you ?') # line break
+print('Days\tTopics\tExercises') # adding tab space or 4 spaces 
+print('Day 1\t3\t5')
+print('Day 2\t3\t5')
+print('Day 3\t3\t5')
+print('Day 4\t3\t5')
+print('This is a backslash  symbol (\\)') # To write a backslash
+print('In every programming language it starts with \"Hello, World!\"') # to write a double quote inside a single quote
+"""
+
+# STRING FORMATS
+"""
+%s - String (or any object with a string representation, like numbers)
+%d - Integers
+%f - Floating point numbers
+"%.number of digitsf" - Floating point numbers with fixed precision
+"""
+#Example
+
+"""
+radius = 10
+pi = 3.14
+area = pi * radius ** 2
+formated_string = 'The area of circle with a radius %d is %.2f.' %(radius, area) # 2 refers the 2 significant digits after the point
+"""
+
+# Python 3 new "format" method in strings
+
+"""
+print('{} + {} = {}'.format(a, b, a + b))
+print('{} - {} = {}'.format(a, b, a - b))
+print('{} * {} = {}'.format(a, b, a * b))
+print('{} / {} = {:.2f}'.format(a, b, a / b)) # limits it to two digits after decimal
+print('{} % {} = {}'.format(a, b, a % b))
+print('{} // {} = {}'.format(a, b, a // b))
+print('{} ** {} = {}'.format(a, b, a ** b))
+"""
+
+# Python 3.6 new "f" method in string
+
+"""
+a = 4
+b = 3
+print(f'{a} + {b} = {a +b}')
+print(f'{a} - {b} = {a - b}')
+print(f'{a} * {b} = {a * b}')
+print(f'{a} / {b} = {a / b:.2f}')
+print(f'{a} % {b} = {a % b}')
+print(f'{a} // {b} = {a // b}')
+print(f'{a} ** {b} = {a ** b}')
+"""
+
+# Python index numbering
+
+"""
+language = 'Python'
+a,b,c,d,e,f = language # unpacking sequence characters into variables
+print(a) # P
+print(b) # y
+print(c) # t
+print(d) # h
+print(e) # o
+print(f) # n
+"""
+
+# Slicing Python Strings
+
+"""
+language = 'Python'
+first_three = language[0:3] # starts at zero index and up to 3 but not include 3
+print(first_three) #Pyt
+last_three = language[3:6]
+print(last_three) # hon
+# Another way
+last_three = language[-3:]
+print(last_three)   # hon
+last_three = language[3:]
+print(last_three)   # hon
+
+greeting = 'Hello, World!'
+print(greeting[::-1]) # !dlroW ,olleH
+
+language = 'Python'
+pto = language[0:6:2] #
+print(pto) # Pto
+"""
+
+# STRING METHODS
+
+"""
+challenge = 'thirty days of python'
+print(challenge.capitalize()) # 'Thirty days of python'
+
+challenge = 'thirty days of python'
+print(challenge.count('y')) # 3
+print(challenge.count('y', 7, 14)) # 1, 
+print(challenge.count('th')) # 2`
+
+challenge = 'thirty days of python'
+print(challenge.endswith('on'))   # True
+print(challenge.endswith('tion')) # False
+
+challenge = 'thirty\tdays\tof\tpython'
+print(challenge.expandtabs())   # 'thirty  days    of      python'
+print(challenge.expandtabs(10)) # 'thirty    days      of        python'
+
+challenge = 'thirty days of python'
+print(challenge.find('y'))  # 16
+print(challenge.find('th')) # 17
+
+challenge = 'thirty days of python'
+print(challenge.rfind('y'))  # 5
+print(challenge.rfind('th')) # 1
+
+first_name = 'Firat'
+last_name = 'Cakir'
+age = 18
+job = 'Student'
+country = 'Turkey'
+sentence = 'I am {} {}. I am a {}. I am {} years old. I live in {}.'.format(first_name, last_name, age, job, country)
+print(sentence) # I am Firat Cakir. I am 18 years old. I am a Student. I live in Turkey.
+
+radius = 10
+pi = 3.14
+area = pi * radius ** 2
+result = 'The area of a circle with radius {} is {}'.format(str(radius), str(area))
+print(result) # The area of a circle with radius 10 is 314
+
+challenge = 'thirty days of python'
+sub_string = 'da'
+print(challenge.index(sub_string))  # 7
+print(challenge.index(sub_string, 9)) # error
+
+challenge = 'thirty days of python'
+sub_string = 'da'
+print(challenge.rindex(sub_string))  # 8
+print(challenge.rindex(sub_string, 9)) # error
+
+challenge = 'ThirtyDaysPython'
+print(challenge.isalnum()) # True
+
+challenge = '30DaysPython'
+print(challenge.isalnum()) # True
+
+challenge = 'thirty days of python'
+print(challenge.isalnum()) # False, space is not an alphanumeric character
+
+challenge = 'thirty days of python 2019'
+print(challenge.isalnum()) # False
+
+challenge = 'thirty days of python'
+print(challenge.isalpha()) # False, space is once again excluded
+challenge = 'ThirtyDaysPython'
+print(challenge.isalpha()) # True
+num = '123'
+print(num.isalpha())      # False
+
+challenge = 'thirty days of python'
+print(challenge.isdecimal())  # False
+challenge = '123'
+print(challenge.isdecimal())  # True
+challenge = '\u00B2'
+print(challenge.isdigit())   # False
+challenge = '12 3'
+print(challenge.isdecimal())  # False, space not allowed
+
+challenge = 'Thirty'
+print(challenge.isdigit()) # False
+challenge = '30'
+print(challenge.isdigit())   # True
+challenge = '\u00B2'
+print(challenge.isdigit())   # True
+
+num = '10'
+print(num.isnumeric()) # True
+num = '\u00BD' # ½
+print(num.isnumeric()) # True
+num = '10.5'
+print(num.isnumeric()) # False
+
+challenge = '30DaysOfPython'
+print(challenge.isidentifier()) # False, because it starts with a number
+challenge = 'thirty_days_of_python'
+print(challenge.isidentifier()) # True
+
+challenge = 'thirty days of python'
+print(challenge.islower()) # True
+challenge = 'Thirty days of python'
+print(challenge.islower()) # False
+
+challenge = 'thirty days of python'
+print(challenge.isupper()) #  False
+challenge = 'THIRTY DAYS OF PYTHON'
+print(challenge.isupper()) # True
+
+web_tech = ['HTML', 'CSS', 'JavaScript', 'React']
+result = ' '.join(web_tech)
+print(result) # 'HTML CSS JavaScript React'
+web_tech = ['HTML', 'CSS', 'JavaScript', 'React']
+result = '# '.join(web_tech)
+print(result) # 'HTML# CSS# JavaScript# React'
+
+challenge = 'thirty days of pythoonnn'
+print(challenge.strip('noth')) # 'irty days of py'
+
+challenge = 'thirty days of python'
+print(challenge.replace('python', 'coding')) # 'thirty days of coding'
+
+challenge = 'thirty days of python'
+print(challenge.split()) # ['thirty', 'days', 'of', 'python']
+challenge = 'thirty, days, of, python'
+print(challenge.split(', ')) # ['thirty', 'days', 'of', 'python']
+
+challenge = 'thirty days of python'
+print(challenge.title()) # Thirty Days Of Python
+
+challenge = 'thirty days of python'
+print(challenge.swapcase())   # THIRTY DAYS OF PYTHON
+challenge = 'Thirty Days Of Python'
+print(challenge.swapcase())  # tHIRTY dAYS oF pYTHON
+
+challenge = 'thirty days of python'
+print(challenge.startswith('thirty')) # True
+
+challenge = '30 days of python'
+print(challenge.startswith('thirty')) # False
+"""
+
+# End of the Day 4
+
+#-------------------
+
+# Start of the Day 5
 
