c097353f6ced801a2194122889f41384333cd357
Fırat Çakır <73741004+FratCR@users.noreply.github.com>
2023-04-22 18:37:27 +0300

    examplemodule.py
    for examples for taking modules

---
+++ examplemodule.py
@@ -0,0 +1,16 @@
+#Creating a Module
+
+def generate_full_name(firstname, lastname):
+    return firstname + ' ' + lastname
+
+def sum_two_nums(number_one, number_two):
+    summed_numbers = number_one + number_two
+    return summed_numbers
+
+def person(id):
+    id = "2246680963"
+    return id
+
+def gravity(gravity_1):
+    gravity_1 = 9.81
+    return gravity_1
